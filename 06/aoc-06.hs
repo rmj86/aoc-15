12380d9b21d430044924e02bc05c71e35fdbda0f
rmj86 <johannes.rehnstrom.1986@gmail.com>
2016-10-08 11:27:06 +0200

    refactoring

--- 06/aoc-06.hs
+++ 06/aoc-06.hs
@@ -3,70 +3,41 @@ import Data.List (foldl')
 {-------------------------------  Part The 1st  -------------------------------}
 
 type Point = (Int, Int)
+type Grid = [[Int]]
 
-lights :: [[Bool]]
-lights = [[False | y<-[0..999]]
-                 | x<-[0..999]]
+lights :: Grid
+lights = [[0 | y <- [0..999]]
+             | x <- [0..999]]
 
-applyInRegion u (x0,y0) (x1,y1) ls = zipWith f [0..] ls
-  where
-    f i xs =
-      if i<x0 then xs else
-        if i>x1 then xs else
-          zipWith g [0..] xs
-    g j x =
-      if j<y0 then x else
-        if j>y1 then x else
-          u x
+mapRegion f (x0,y0) (x1,y1) rows = zipWith u [0..] rows
+  where u i row = if i < x0  ||  x1 < i
+                  then row
+                  else zipWith v [0..] row
+        v j e   = if j < y0  ||  y1 < j
+                  then e
+                  else f e
 
+apply :: Grid -> (String, Point, Point) -> Grid
+apply ls (c, p0, p1) = case c of
+        "toggle"  -> mapRegion (1-)      p0 p1 ls
+        "turnon"  -> mapRegion (const 1) p0 p1 ls
+        "turnoff" -> mapRegion (const 0) p0 p1 ls
 
-toggle :: Point -> Point -> [[Bool]] -> [[Bool]]
-toggle p0 p1 ls = applyInRegion not p0 p1 ls
+applyAll cs init = foldl' apply init cs
 
-turn :: Bool -> Point -> Point -> [[Bool]] -> [[Bool]]
-turn b p0 p1 ls = applyInRegion (\_->b) p0 p1 ls
-
-apply :: [[Bool]] -> (String, Point, Point) -> [[Bool]]
-apply ls (c, p0, p1)
- | c=="turnon" = turn True p0 p1 ls
- | c=="turnoff" = turn False p0 p1 ls
- | c=="toggle" = toggle p0 p1 ls
- | otherwise = error ""
-
-apply_all cs = foldl' apply lights cs
-
-count_them ls = sum $ map (foldl' tick 0) ls
-  where tick n b = if b then n+1 else n
-
-
-solution1 cs = count_them $ apply_all cs
+solution1 cs = sum . map sum . applyAll cs $ lights
 
 {-------------------------------  Part The 2nd  -------------------------------}
 
-lights2 :: [[Int]]
-lights2 = [[0 | y<-[0..999]]
-              | x<-[0..999]]
+apply2 :: Grid -> (String, Point, Point) -> Grid
+apply2 ls (c, p0, p1) = case c of
+        "toggle"  -> mapRegion (+2)               p0 p1 ls
+        "turnon"  -> mapRegion (+1)               p0 p1 ls
+        "turnoff" -> mapRegion (\v-> max 0 (v-1)) p0 p1 ls
 
-turnon2 p0 p1 ls = applyInRegion (+1) p0 p1 ls
-toggle2 p0 p1 ls = applyInRegion (+2) p0 p1 ls
-turnoff2 p0 p1 ls = applyInRegion (\v->max 0 (v-1)) p0 p1 ls
+solution2 cs = sum . map sum . foldl' apply2 lights $ cs
 
-
-apply2 :: [[Int]] -> (String, Point, Point) -> [[Int]]
-apply2 ls (c, p0, p1)
- | c=="turnon" = turnon2 p0 p1 ls
- | c=="turnoff" = turnoff2 p0 p1 ls
- | c=="toggle" = toggle2 p0 p1 ls
- | otherwise = error ""
-
-apply_all2 cs = foldl' apply2 lights2 cs
-
-add_them ls = sum $ map sum ls
-
-solution2 cs = add_them $ apply_all2 cs
-
-
---- IO ---
+{------------------------------------  IO  ------------------------------------}
 
 parse_p :: String -> Point
 parse_p s = read $ "("++s++")"
